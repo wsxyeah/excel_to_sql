8d5b80ce288013428901ee94d879cbca1fd053de
Shoaxing <wsxking@yeah.net>
2015-06-16 20:45:50 +0800

    improve distinguishing method for different column datatypes

--- excel_to_sql.py
+++ excel_to_sql.py
@@ -13,10 +13,15 @@ data = xlrd.open_workbook(filename)
 with open('insert.sql', 'w') as out_file:
 	for sheet in data.sheets():
 		table_name = sheet.name
-		cols = map(lambda item: '`' + item + '`', sheet.row_values(0))
+		cols = map(lambda item: ('[' + item[:-4] + ']') if item.endswith('.NaN') else ('[' + item + ']'), sheet.row_values(0))
+		nan_flags = map(lambda item: True if item.endswith('.NaN') else False, sheet.row_values(0))
+
 		out_file.write('-- ' + table_name + '\r\n')
 		for i in range(1,sheet.nrows):
-			items = map(lambda item: str(item.value) if re.match(r'^[\d\.]+$', str(item.value)) else '\'' + str(item.value) + '\'', sheet.row(i))
-			insert_sql = 'INSERT INTO %s (%s) VALUES(%s);' % (table_name, ', '.join(cols), ', '.join(items))
+			row_values = sheet.row_values(i)
+			for j in range(0,len(row_values)):
+				if nan_flags[j] or not re.match(r'^[\d\.]+$', str(row_values[j])):
+					row_values[j] = '\'' + str(row_values[j]) + '\''
+			insert_sql = 'INSERT INTO [%s] (%s) VALUES(%s);' % (table_name, ', '.join(cols), ', '.join(row_values))
 			out_file.write(insert_sql + '\r\n')
 print 'SQL file was gernarated successfully! See insert.sql.'
